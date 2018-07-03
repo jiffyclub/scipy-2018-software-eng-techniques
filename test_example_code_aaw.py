15de7d6718d66699b48c5d1275bed8e780acdb77
Alyssa Whitwell <alyssa.whitwell@cloverhealth.com>
2018-07-03 13:26:21 -0700

    Spacing

--- test_example_code_aaw.py
+++ test_example_code_aaw.py
@@ -6,7 +6,6 @@ def test_load_file():
     """Test that sf_restaurant_scores_subset.csv loads as expected."""
     data = eca.load_file('data/sf_restaurant_scores_subset.csv')
 
-
     # Assert that there are 10 rows of data in the dataset.
     assert len(data) == 10
     # Assert that for a given row, it has 17 datapoints.
