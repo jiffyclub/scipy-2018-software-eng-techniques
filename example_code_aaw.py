eba2f3b153dbb58a84d3e859efc0263c4a45025e
Alyssa Whitwell <alyssa.whitwell@cloverhealth.com>
2018-07-03 13:25:16 -0700

    Return data as list of dicts

--- example_code_aaw.py
+++ example_code_aaw.py
@@ -8,21 +8,9 @@ def load_file(filename):
     Read dataset using filepath, and return data as a dictionary of lists.
       - Example returned dataset: {'col1_name': [1, 2, 3]}
     """
-    data = {}
-
     # Open dataset
     with open(filename) as file:
         reader = csv.DictReader(file)
-
-        # Initiate the dictionary with column names as keys.
-        # Values for every key will be an empty list.
-        for column_name in reader.fieldnames:
-            data[column_name] = []
-
-        # Loop through rows and append each row of data.
-        # Each row will be appended to the list in the key-value store.
-        for row in reader:
-            for column_name in reader.fieldnames:
-                data[column_name].append(row[column_name])
-
+        data = list(reader)
     return data
+
--- test_example_code_aaw.py
+++ test_example_code_aaw.py
@@ -6,7 +6,8 @@ def test_load_file():
     """Test that sf_restaurant_scores_subset.csv loads as expected."""
     data = eca.load_file('data/sf_restaurant_scores_subset.csv')
 
-    # Assert all columns have a key.
-    assert len(data) == 17
+
     # Assert that there are 10 rows of data in the dataset.
-    assert len(data['business_id']) == 10
+    assert len(data) == 10
+    # Assert that for a given row, it has 17 datapoints.
+    assert len(data[0]) == 17
